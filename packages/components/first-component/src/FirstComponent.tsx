31cea6397c7f4da90c26a418ac0b13006725b8e1
yichangmin <changmin.yi@myrealtrip.com>
2022-11-16 19:10:29 +0900

    testtest

--- packages/components/first-component/src/FirstComponent.tsx
+++ packages/components/first-component/src/FirstComponent.tsx
@@ -9,6 +9,7 @@ export const FirstComponent: React.FC<FirstComponentProps> = ({ name, descriptio
   console.log('test');
   console.log('test2');
   console.log('test3');
+  console.log('test4');
   return (
     <div>
       <p>{name}</p>
